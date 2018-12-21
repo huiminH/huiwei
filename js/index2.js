8c1542b1133cd513929d2d8a9adf70bccf8e1185
“e <2478460468@qq.com>
2018-12-21 12:16:49 +0800

    提交

--- js/index2.js
+++ js/index2.js
@@ -12,7 +12,7 @@ window.onload = function(){
            for(var i = 0;i < res[key].list.length;i++){
                var cur = res[key].list[i];
                str+=`<li class="duct-d" id="${cur.id}">
-                        <a href="#" class="duct-d1">
+                        <a href="details.html" class="duct-d1">
                             <p class="duct-d1-s">
                             <img src="${cur.src}"/></p>
                             <div class="duct-title">${cur.p1}</div>
@@ -42,7 +42,7 @@ window.onload = function(){
                for(var i = 0;i < res[key].list.length;i++){
                    var cur1 = res[key].list[i];
                     str1+=`<li class="lity" id="${cur1.id}">
-                              <a href="#" class="a1">
+                              <a href="details.html" class="a1">
                                 <div class="lity1"><p class="lity-img"><img src="${cur1.src}"/></p>
                                 <p class="p-desc">${cur1.p1}</p></div><div class="p-title">${cur1.p2}</div>
                                 <p class="p-price">${cur1.p3}</p>
@@ -64,13 +64,13 @@ window.onload = function(){
         success:function(res){
             // console.log(res)
             var str2= "";
-            str2 = '<li class="duct-d"><a href="#"><img class="img1" src="image/2-4-1.png" alt=""></a></li>';
+            str2 = '<li class="duct-d"><a href="details.html"><img class="img1" src="image/2-4-1.png" alt=""></a></li>';
             for(var key in res){
             //    console.log(key);
                for(var i = 0;i < res[key].list.length;i++){
                    var cur2 = res[key].list[i];
                    str2+=`<li class="duct-d" id="${cur2.id}">
-                            <a href="#" class="duct-d1">
+                            <a href="details.html" class="duct-d1">
                                 <p class="duct-d1-s"><img src="${cur2.src}"/></p>
                                 <div class="duct-title">${cur2.p1}</div>
                                 <p class="duct-desc">${cur2.p2}</p>
@@ -94,11 +94,11 @@ window.onload = function(){
             var str3= "";
             for(var key in res){
             //    console.log(key);
-            str3 = '<li class="duct-d"><a href="#"><img class="img1" src="image/computer.png" alt=""></a></li>';
+            str3 = '<li class="duct-d"><a href="details.html"><img class="img1" src="image/computer.png" alt=""></a></li>';
             for(var i = 0;i < res[key].list.length;i++){
                 var cur3 = res[key].list[i];
                     str3+=`<li class="duct-d id="${cur3.id}">
-                                <a href="#" class="duct-d1"><p class="duct-d1-s">
+                                <a href="details.html" class="duct-d1"><p class="duct-d1-s">
                                     <img src="${cur3.src}"/></p>
                                     <div class="duct-title">${cur3.p1}</div>
                                     <p class="duct-desc">${cur3.p2}</p>
@@ -121,13 +121,13 @@ window.onload = function(){
         cache : true,   
         success:function(res){
             var str4= "";
-            str4 = '<li class="duct-d"><a href="#"><img class="img1" src="image/flag.png" alt=""></a></li>';
+            str4 = '<li class="duct-d"><a href="details.html"><img class="img1" src="image/flag.png" alt=""></a></li>';
             for(var key in res){
             //    console.log(key);
             for(var i = 0;i < res[key].list.length;i++){
                 var cur4 = res[key].list[i];
                     str4+=`<li class="duct-d id="${cur4.id}">
-                                <a href="#" class="duct-d1"><p class="duct-d1-s">
+                                <a href="details.html" class="duct-d1"><p class="duct-d1-s">
                                     <img src="${cur4.src}"/></p>
                                     <div class="duct-title">${cur4.p1}</div>
                                     <p class="duct-desc">${cur4.p2}</p>
@@ -151,13 +151,13 @@ window.onload = function(){
         success:function(res){
             // console.log(res);
             var str5= "";
-            str5 = '<li class="duct-d" id="wr"><a href="#"><img class="img1" src="image/smart.png" alt=""></a></li>';
+            str5 = '<li class="duct-d" id="wr"><a href="details.html"><img class="img1" src="image/smart.png" alt=""></a></li>';
             for(var key in res){
                 for(var i = 0;i < res[key].list.length;i++){
                     var cur5 = res[key].list[i];
                     // console.log(cur5);
                         str5+=`<li class="duct-d id="${cur5.id}">
-                                    <a href="#" class="duct-d1"><p class="duct-d1-s">
+                                    <a href="details.html" class="duct-d1"><p class="duct-d1-s">
                                         <img src="${cur5.src}"/></p>
                                         <div class="duct-title">${cur5.p1}</div>
                                         <p class="duct-desc">${cur5.p2}</p>
@@ -186,7 +186,7 @@ window.onload = function(){
                     for(var i = 0; i < res[key].list.length;i++){
                                 var cur6 = res[key].list[i];
                                 str6+=`<li class="wear">
-                                    <a href="#"><div class="wear-info"><p class="wear-img">
+                                    <a href="details.html"><div class="wear-info"><p class="wear-img">
                                     <img src="${cur6.src}"/></p>
                                     <p class="wear-desc">${cur6.p1}</p></div>
                                     <div class="wear-title">${cur6.p2}</div>
@@ -209,13 +209,13 @@ window.onload = function(){
         success:function(res){
             // console.log(res);
             var str7= "";
-            str7 = '<li class="duct-d" id="wr"><a href="#"><img class="img1" src="image/home.png" alt=""></a></li>';
+            str7 = '<li class="duct-d" id="wr"><a href="details.html"><img class="img1" src="image/home.png" alt=""></a></li>';
             for(var key in res){
                 for(var i = 0;i < res[key].list.length;i++){
                     var cur7 = res[key].list[i];
                     // console.log(cur5);
                         str7+=`<li class="duct-d id="${cur7.id}">
-                                    <a href="#" class="duct-d1"><p class="duct-d1-s">
+                                    <a href="details.html" class="duct-d1"><p class="duct-d1-s">
                                         <img src="${cur7.src}"/></p>
                                         <div class="duct-title">${cur7.p1}</div>
                                         <p class="duct-desc">${cur7.p2}</p>
@@ -266,13 +266,13 @@ window.onload = function(){
         success:function(res){
             // console.log(res);
             var str9= "";
-            str9 = '<li class="duct-d" id="wr"><a href="#"><img class="img1" src="image/sell.png" alt=""></a></li>';
+            str9 = '<li class="duct-d" id="wr"><a href="details.html"><img class="img1" src="image/sell.png" alt=""></a></li>';
             for(var key in res){
                 for(var i = 0;i < res[key].list.length;i++){
                     var cur9 = res[key].list[i];
                     // console.log(cur5);
                         str9 +=`<li class="duct-d id="${cur9.id}">
-                                    <a href="#" class="duct-d1"><p class="duct-d1-s">
+                                    <a href="details.html" class="duct-d1"><p class="duct-d1-s">
                                         <img src="${cur9.src}"/></p>
                                         <div class="duct-title">${cur9.p1}</div>
                                         <p class="duct-desc">${cur9.p2}</p>
@@ -301,7 +301,7 @@ window.onload = function(){
                     for(var i = 0; i < res[key].list.length;i++){
                                 var res1 = res[key].list[i];
                                 num+=`<li class="wear">
-                                    <a href="#"><div class="wear-info"><p class="wear-img">
+                                    <a href="details.html"><div class="wear-info"><p class="wear-img">
                                     <img src="${res1.src}"/></p>
                                     <p class="wear-desc">${res1.p1}</p></div>
                                     <div class="wear-title">${res1.p2}</div>
@@ -324,13 +324,13 @@ window.onload = function(){
         success:function(res){
             // console.log(res);
             var num1= "";
-            num1 = '<li class="duct-d" id="wr"><a href="#"><img class="img1" src="image/brand.png" alt=""></a></li>';
+            num1 = '<li class="duct-d" id="wr"><a href="details.html"><img class="img1" src="image/brand.png" alt=""></a></li>';
             for(var key in res){
                 for(var i = 0;i < res[key].list.length;i++){
                     var res2 = res[key].list[i];
                     // console.log(cur5);
                         num1 +=`<li class="duct-d id="${res2.id}">
-                                    <a href="#" class="duct-d1"><p class="duct-d1-s">
+                                    <a href="details.html" class="duct-d1"><p class="duct-d1-s">
                                         <img src="${res2.src}"/></p>
                                         <div class="duct-title">${res2.p1}</div>
                                         <p class="duct-desc">${res2.p2}</p>
@@ -359,7 +359,7 @@ window.onload = function(){
                     for(var i = 0; i < res[key].list.length;i++){
                                 var res3 = res[key].list[i];
                                 num2+=`<li class="wear">
-                                    <a href="#"><div class="wear-info"><p class="wear-img">
+                                    <a href="details.html"><div class="wear-info"><p class="wear-img">
                                     <img src="${res3.src}"/></p>
                                     <p class="wear-desc">${res3.p1}</p></div>
                                     <div class="wear-title">${res3.p2}</div>
